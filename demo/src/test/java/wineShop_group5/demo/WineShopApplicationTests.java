22ba911055b440ed0082c0a99da72faf3ad7e901
GemmaGft <gamr@gft.com>
2022-08-23 12:05:07 +0200

    arreglo main

---
+++ demo/src/test/java/wineShop_group5/demo/WineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package wineShop_group5.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

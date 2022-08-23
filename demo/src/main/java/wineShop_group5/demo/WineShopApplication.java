ebf5b5f5a31d7362111d60d3d2339560066cbadb
GemmaGft <109504324+GemmaGft@users.noreply.github.com>
2022-08-23 10:41:52 +0200

    Add files via upload
    Project created

---
+++ demo/src/main/java/wineShop_group5/demo/WineShopApplication.java
@@ -0,0 +1,13 @@
+package wineShop_group5.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineShopApplication.class, args);
+	}
+
+}

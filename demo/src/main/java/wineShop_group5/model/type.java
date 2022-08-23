e8ca20d4b406400e387a19e301f3bfc605447ae9
marcal.gft <mlig@gft.com>
2022-08-23 12:40:04 +0200

    marsal_type

---
+++ demo/src/main/java/wineShop_group5/model/type.java
@@ -0,0 +1,32 @@
+package wineShop_group5.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "type")
+public class type {
+	
+	@Id
+	private int id;
+	
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}

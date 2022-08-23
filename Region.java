427f940ae4c90aa93c389fd17a9c3416f8d63250
Jilz97 <109506312+Jilz97@users.noreply.github.com>
2022-08-23 11:58:13 +0200

    Add files via upload

---
+++ Region.java
@@ -0,0 +1,35 @@
+package wineShop_group5.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Region {
+	
+	@Id
+	private int region_id;
+	
+	private String region_name;
+	private String country_name;
+	
+	
+	public int getRegion_id() {
+		return region_id;
+	}
+	public void setRegion_id(int region_id) {
+		this.region_id = region_id;
+	}
+	public String getRegion_name() {
+		return region_name;
+	}
+	public void setRegion_name(String region_name) {
+		this.region_name = region_name;
+	}
+	public String getCountry_name() {
+		return country_name;
+	}
+	public void setCountry_name(String country_name) {
+		this.country_name = country_name;
+	}
+	
+}

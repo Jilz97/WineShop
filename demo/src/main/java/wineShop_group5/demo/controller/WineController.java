d2348b8168761712d71a014706957050450e2930
GemmaGft <gamr@gft.com>
2022-08-24 09:27:36 +0200

    crud wine

--- demo/src/main/java/wineShop_group5/demo/controller/WineController.java
+++ demo/src/main/java/wineShop_group5/demo/controller/WineController.java
@@ -3,8 +3,12 @@ package wineShop_group5.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +30,31 @@ public class WineController {
 	@GetMapping("/{id}")
 	public Wine getWineId(@PathVariable Integer id) throws Exception{
 		return wineRepository.findById(id).orElseThrow(()-> new Exception("Not found"));
+	}
+
+	@PostMapping("/create")
+	public Wine createWine(@RequestBody Wine wine){
+		wineRepository.save(wine);
+		return wine;
+	}
+
+	@PutMapping("/update/{id}")
+	public Wine updateWine(@PathVariable int id, @RequestBody Wine wine) throws Exception{
+		Wine wine1 = getWineId(id);
+		wine1.setName(wine.getName());
+		wine1.setYear(wine.getYear());
+		wine1.setRating(wine.getRating());
+		wine1.setNum_reviews(wine.getNum_reviews());
+		wine1.setBody(wine.getBody());
+		wine1.setPrice(wine.getPrice());
+		wine1.setAcidity(wine.getAcidity());
+		wineRepository.save(wine1);
+		return wine1;
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public String deleteWine(@PathVariable int id){
+		wineRepository.deleteById(id);
+		return "Wine " + id + " has been deleted";
 	}
 }

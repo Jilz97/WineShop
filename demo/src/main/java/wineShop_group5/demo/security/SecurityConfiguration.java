4e233367a6deda483140ecd73079b15e488406e9
GemmaGft <gamr@gft.com>
2022-08-25 13:22:07 +0200

    up

--- demo/src/main/java/wineShop_group5/demo/security/SecurityConfiguration.java
+++ demo/src/main/java/wineShop_group5/demo/security/SecurityConfiguration.java
@@ -1,18 +1,14 @@
 package wineShop_group5.demo.security;
 
-import java.util.Optional;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.DefaultSecurityFilterChain;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
--- demo/src/main/java/wineShop_group5/demo/services/WineryService.java
+++ demo/src/main/java/wineShop_group5/demo/services/WineryService.java
@@ -1,8 +1,6 @@
 package wineShop_group5.demo.services;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;

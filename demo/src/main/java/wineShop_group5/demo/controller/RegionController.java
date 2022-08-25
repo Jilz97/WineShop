79ab4e43da73845d94b61a680a30fb5064f023b3
Jilz97 <jilz@gft.com>
2022-08-25 12:43:31 +0200

    authentication and regions

--- demo/src/main/java/wineShop_group5/demo/controller/RegionController.java
+++ demo/src/main/java/wineShop_group5/demo/controller/RegionController.java
@@ -1,55 +1,52 @@
 package wineShop_group5.demo.controller;
 
+import java.rmi.ServerException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 import wineShop_group5.demo.model.Region;
-import wineShop_group5.demo.repository.RegionRepository;
+import wineShop_group5.demo.services.RegionService;
 
 @RestController
 @RequestMapping("/api/region")
 public class RegionController {
 	
 	@Autowired
-	RegionRepository regionRepository;
+	RegionService regionService;
 	
 
 	@GetMapping("/all")
 	public List<Region> getAllRegion() {
-		return regionRepository.findAll();
+		return regionService.getAllRegion();
 		
 	}
 	@GetMapping("/{id}")
 	public Region getRegionId(@PathVariable Integer id) throws Exception{
-		return regionRepository.findById(id).orElseThrow(()-> new Exception("Not found"));
+		return regionService.getRegionId(id);
+	}
+		
+	@PostMapping("/create")
+	public Region createRegion(@RequestBody Region region) throws ServerException {
+		return regionService.saveRegion(region);
 	}
 	
-	@PostMapping("/create")
-	public Region postRegion(@RequestBody Region region) {
-		return regionRepository.save(region);
-	}
 	@PutMapping("/update/{id}")
-	public Region updateRegion(@PathVariable int id, @RequestBody Region region) throws Exception{
-		Region region1 = getRegionId(id);
-		region1.setId(id);
-		region1.setCountry(region.getCountry());
-		region1.setName(region.getName());
-		return region1;
+	public Region updateRegion(@PathVariable Integer id,@RequestBody Region region) throws Exception{
+		return regionService.updateRegion(id,region);
 	}
+	
 	@DeleteMapping("/delete/{id}")
 	public String deleteRegion(@PathVariable Integer id) {
-		regionRepository.deleteById(id);
+		regionService.deleteRegion(id);
 		return "Region"+ id + "deleted";
 	}
 	
--- demo/src/test/java/wineShop_group5/demo/RegionControllerTest.java
+++ demo/src/test/java/wineShop_group5/demo/RegionControllerTest.java
@@ -5,18 +5,29 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import wineShop_group5.demo.controller.RegionController;
+import wineShop_group5.demo.model.Region;
+import wineShop_group5.demo.repository.RegionRepository;
+import wineShop_group5.demo.services.RegionService;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -25,9 +36,12 @@ class RegionControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 	
-	@MockBean
-	private Region
-
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	@Mock
+	RegionService regionService;
+	
 	@Test
 	void all() throws Exception {
 		mockMvc.perform(get("/api/region/")
@@ -49,11 +63,43 @@ class RegionControllerTest {
 		});
 	}
 
-	@Test 
-	void puttest() throws Exception{
+	@Test
+	void addRegionTest() throws Exception{
+		Region region = new Region();
+		region.setName("prova");
+		region.setId(200);
 		
+		Mockito.when(regionService.saveRegion(region)).thenReturn(region);
+				
+		ResultActions response = mockMvc.perform(post("/api/region/create/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(region)));
 		
+		response.andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$.name", is("prova")));
 	}
+
+	@Test
+	void updateRegionTest() throws Exception{
+		Region region = new Region();
+		region.setName("prova");
+		region.setId(100);
+		Mockito.when(regionService.updateRegion(100,region)).thenReturn(region);
+		
+		
+		
+		ResultActions response=mockMvc.perform(put("/api/region/update")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(region)));
+		
+		response.andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$.name", is("prova")));
+				
+	}
+
+	@Test
+	void deleteRegionTest() throws Exception{
+		
 	
+		mockMvc.perform(delete("/api/region/delete/200").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+	}
 	
 }

420251a68e2e7d72e0f90df68c5e95c9b9b5861c
GemmaGft <gamr@gft.com>
2022-08-25 16:30:20 +0200

    up

--- demo/src/test/java/wineShop_group5/demo/WineTest.java
+++ demo/src/test/java/wineShop_group5/demo/WineTest.java
@@ -50,17 +50,16 @@ class WineTest {
 		mockMvc.perform(MockMvcRequestBuilders.get("/api/wine/all").contentType("application/json"))
 				.andExpect(status().isOk());
 	}
-/*
+
 	// Test get Wine by Id
 	@Test
 	void idOne() throws Exception {
-		//wine.setId(11);
-		//given(wineServices.getWineId(11)).willReturn(wine);
+		wine.setId(11);
+		given(wineServices.getWineId(11)).willReturn(wine);
 		mockMvc.perform(get("/api/wine/11")
 				.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(jsonPath("$.id", is(11)));
 	}
-*/
 	// Test create
 	@Test
 	@WithMockUser(username = "user", roles= {"USER"})
